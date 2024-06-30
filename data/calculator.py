6f4d95ab5a0ed0a20485b60ab005dbeb5f387b21
rognit <pierre.brg314@gmail.com>
2024-06-30 19:44:05 +0200

    calculator: edges & vertices

--- data/calculator.py
+++ data/calculator.py
@@ -20,66 +20,117 @@ pop_df = pd.read_csv('../resources/test_pop_filtered.csv')
 
 
 
-def round_down_to_nearest_200(value):
-    return int(value // 200 * 200)
+def add_color_to_edges(buffer_distance=200,
+                       square_size=200,
+                       square_size_squared=40000,
+                       orange_threshold=300,
+                       red_threshold=3000, ):
 
-def get_square(north_point, east_point):
-    return round_down_to_nearest_200(north_point), round_down_to_nearest_200(east_point)
+    def get_density(x, y):
+        return pop_df.loc[(pop_df['north'] == y) & (pop_df['east'] == x), 'density'].values[0]
 
-def get_density(north_square, east_point):
-    return pop_df.loc[(pop_df['north'] == north_square) & (pop_df['east'] == east_point), 'density'].values[0]
+    def round_down_to_nearest(value):
+        return int(value // square_size_squared)
 
-def get_line(x1, y1, x2, y2):
-    a = (y2 - y1) / (x2 - x1)
-    b = y1 - a * x1
-    return lambda x: a * x + b
-
-def get_squares_from_line(north_point_1, east_point_1, north_point_2, east_point_2):
-
-    north_square_1, east_square_1 = get_square(north_point_1, east_point_1)
-    north_square_2, east_square_2 = get_square(north_point_2, east_point_2)
-
-    f = get_line(east_point_1, north_point_1, east_point_2, north_point_2)
-    direction = 1 if east_point_1 < east_point_2 else -1
-
-    squares = [(north_square_1, east_square_1)]
-    old_north_square = north_square_1
-    for east_square in range(east_square_1 + 1, east_square_2 + 1, direction * 200):
-        north_square = round_down_to_nearest_200(f(east_square))
-        for crossed_north_square in range(old_north_square, north_square + 1, 200):
-            squares.append(get_square(crossed_north_square, east_square))
-        old_north_square = north_square
-
-    return list(set(squares))
+    def get_square(x, y):
+        return round_down_to_nearest(x), round_down_to_nearest(y)
 
 
-def calculate_parallel_lines(vector, d):
-    x1, y1, x2, y2 = vector
-    dx, dy = x2 - x1, y2 - y1
-    length = math.sqrt(dx ** 2 + dy ** 2)
+    def get_colors_from_section(section):
 
-    # Normalized normal vectors
-    n1_unit_x, n1_unit_y = -dy / length, dx / length
-    n2_unit_x, n2_unit_y = dy / length, -dx / length
+        def get_squares_from_edge(edge):
+            x1, y1, x2, y2 = edge
+            dx, dy = x2 - x1, y2 - y1
+            length = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+            edge_squares = set()
 
-    # First parallel line
-    x1_l1 = x1 + d * n1_unit_x
-    y1_l1 = y1 + d * n1_unit_y
-    x2_l1 = x2 + d * n1_unit_x
-    y2_l1 = y2 + d * n1_unit_y
+            def get_line(x1_local, y1_local, x2_local, y2_local):
+                dx_local, dy_local = x2_local - x1_local, y2_local - y1_local
+                a = dy_local / dx_local
+                b = y1_local - a * x1_local
+                return lambda x: a * x + b
 
-    # Second parallel line
-    x1_l2 = x1 + d * n2_unit_x
-    y1_l2 = y1 + d * n2_unit_y
-    x2_l2 = x2 + d * n2_unit_x
-    y2_l2 = y2 + d * n2_unit_y
+            def get_squares_from_line(east_point_1, north_point_1, east_point_2, north_point_2):
+                north_square_1, east_square_1 = get_square(north_point_1, east_point_1)
+                north_square_2, east_square_2 = get_square(north_point_2, east_point_2)
 
-    return (x1_l1, y1_l1, x2_l1, y2_l1), (x1_l2, y1_l2, x2_l2, y2_l2)
+                f = get_line(east_point_1, north_point_1, east_point_2, north_point_2)
+                direction = 1 if east_point_1 < east_point_2 else -1
 
-def get_squares_from_edge(north_point_1, east_point_1, north_point_2, east_point_2, buffer_distance):
-    edge_f = get_line(east_point_1, north_point_1, east_point_2, north_point_2)
+                output_squares = [(north_square_1, east_square_1)]
+                old_north_square = north_square_1
+                for east_square in range(east_square_1 + 1, east_square_2 + 1, direction * square_size):
+                    north_square = round_down_to_nearest(f(east_square))
+                    for crossed_north_square in range(old_north_square, north_square + 1, square_size):
+                        output_squares.append(get_square(crossed_north_square, east_square))
+                    old_north_square = north_square
 
-def get_square_from_vertex(north_point, east_point):
-    return get_square(north_point, east_point)
+                return list(set(output_squares))
+
+            def get_boundaries():
+                # Normalized normal vectors
+                xn1, yn1 = -dy / length, dx / length
+                xn2, yn2 = dy / length, -dx / length
+
+                return ((x1 + buffer_distance * xn1,  # Line 1
+                         y1 + buffer_distance * yn1,
+                         x2 + buffer_distance * xn1,
+                         y2 + buffer_distance * yn1),
+                        (x1 + buffer_distance * xn2,  # Line 2
+                         y1 + buffer_distance * yn2,
+                         x2 + buffer_distance * xn2,
+                         y2 + buffer_distance * yn2))
+
+
+
+            (x1_l1, y1_l1, x2_l1, y2_l1), (x1_l2, y1_l2, x2_l2, y2_l2) = get_boundaries()
+
+            edge_squares.update(get_squares_from_line(x1_l1, y1_l1, x2_l1, y2_l1))
+            edge_squares.update(get_squares_from_line(x1_l2, y1_l2, x2_l2, y2_l2))
+
+            number_of_lines = int(buffer_distance * 2 / square_size)
+            for i in range(1, number_of_lines):
+                offset_x = i * square_size * (x2_l1 - x1_l1) / length
+                offset_y = i * square_size * (y2_l1 - y1_l1) / length
+                edge_squares.update(get_squares_from_line(x1_l1 + offset_x, y1_l1 + offset_y, x2_l1 + offset_x, y2_l1 + offset_y))
+
+            return edge_squares
+
+        def get_square_from_vertex(vertex):
+            x, y = vertex
+
+            def is_within_distance(x_corner, y_corner, x_vertex, y_vertex):
+                return math.sqrt((x_corner - x_vertex) ** 2 + (y_corner - y_vertex) ** 2) <= buffer_distance
+
+            def get_squares_from_corner(x_corner, y_corner):
+                return ((x_corner, y_corner),
+                        (x_corner - square_size, y_corner),
+                        (x_corner, y_corner - square_size),
+                        (x_corner - square_size, y_corner - square_size))
+
+
+            x_initial_square, y_initial_square = get_square(x, y)
+            edge_corners = set()
+            n_ring = 0
+            while n_ring * square_size <= buffer_distance:
+                for i in range(-n_ring, n_ring + 1):
+                    for j in range(-n_ring, n_ring + 1):
+                        corners = [
+                            (x_initial_square + j * square_size, y_initial_square + i * square_size),
+                            (x_initial_square + j * square_size, y_initial_square + (i + 1) * square_size),
+                            (x_initial_square + (j + 1) * square_size, y_initial_square + i * square_size),
+                            (x_initial_square + (j + 1) * square_size, y_initial_square + (i + 1) * square_size)
+                        ]
+                        for x_corner, y_corner in corners:
+                            edge_corners.add((x_corner, y_corner))
+                n_ring += 1
+
+            edge_squares = set()
+            for x_corner, y_corner in edge_corners:
+                if is_within_distance(x_corner, y_corner, x, y):
+                    edge_squares.update(get_squares_from_corner(x_corner, y_corner))
+
+            return edge_squares
+
 
 
